25b9755b346d7fdb7fd9f49f4dcc0cc7c270f967
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-11-01 12:12:26 -0600

    offset and key class tests pass, 100% covered

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -15,6 +15,15 @@ class OffsetTest < Minitest::Test
 
   def test_it_has_time
     offset = Offset.new
+    assert_equal '9924', offset.last4
     assert_equal 4, offset.last4.length
+  end
+
+  def test_offsets_return_each_digit_of_last_4
+    offset = Offset.new
+    assert_equal 9, offset.a_offset
+    assert_equal 9, offset.b_offset
+    assert_equal 2, offset.c_offset
+    assert_equal 4, offset.d_offset
   end
 end
