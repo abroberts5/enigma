e2412729ed1c3b0da7d08189bf4cc5c54546217e
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-11-03 17:50:50 -0600

    test it has attributes

--- test/scrambled_test.rb
+++ test/scrambled_test.rb
@@ -6,4 +6,10 @@ class ScrambledTest < Minitest::Test
     scrambled_eggs = Scrambled.new('12345678', 'hello world')
     assert_instance_of Scrambled, scrambled_eggs
   end
+
+  def test_it_has_attributes
+    scrambled_eggs = Scrambled.new('12345678', 'hello world')
+    assert_equal '12345678', scrambled_eggs.rotation
+    assert_equal 'hello world', scrambled_eggs.phrase
+  end
 end
