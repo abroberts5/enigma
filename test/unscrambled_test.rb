cf0523487e64178048f5b359a36645e25e2c84f6
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-11-04 09:21:48 -0700

    update foor rotation test

--- test/unscrambled_test.rb
+++ test/unscrambled_test.rb
@@ -7,8 +7,14 @@ class UnScrambledTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    scrambled = UnScrambled.new('12345678', 'poiurunfksr')
-    assert_equal '12345678', scrambled.rotation
-    assert_equal 'poiurunfksr', scrambled.phrase
+    loosey = UnScrambled.new('12345678', 'poiurunfksr')
+    assert_equal '12345678', loosey.rotation
+    assert_equal 'poiurunfksr', loosey.phrase
+  end
+
+  def test_it_takes_rotation_and_breaks_it_up_from_final_class
+    loosey = UnScrambled.new('12345678', 'poiurunfksr')
+    expected = ['12', '34', '56', '78']
+    assert_equal expected, loosey.rotation_array
   end
 end
