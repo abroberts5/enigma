9e42ccc50a50a3e5475c17b834eef2c4537c1c4b
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-11-04 09:20:01 -0700

    passed test for it has attributes

--- lib/unscrambled.rb
+++ lib/unscrambled.rb
@@ -1,3 +1,15 @@
 class UnScrambled
-
+  attr_reader     :rotation,
+                  :phrase
+                  
+  def initialize(final_rotation, phrase)
+    @letters = ["a", "b", "c", "d", "e",
+                "f", "g", "h", "i", "j",
+                "k", "l", "m", "n", "o",
+                "p", "q", "r", "s", "t",
+                "u", "v", "w", "x", "y",
+                "z", " "]
+    @rotation   = final_rotation
+    @phrase     = phrase
+  end
 end
--- test/unscrambled_test.rb
+++ test/unscrambled_test.rb
@@ -2,7 +2,13 @@ require_relative 'test_helper'
 
 class UnScrambledTest < Minitest::Test
   def test_it_exists
-    loosey = UnScrambled.new('12345678', 'hello world')
+    loosey = UnScrambled.new('12345678', 'poiurunfksr')
     assert_instance_of UnScrambled, loosey
+  end
+
+  def test_it_has_attributes
+    scrambled = UnScrambled.new('12345678', 'poiurunfksr')
+    assert_equal '12345678', scrambled.rotation
+    assert_equal 'poiurunfksr', scrambled.phrase
   end
 end
